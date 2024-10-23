75e316c4a64fe29800254fcf3c9a412114bd4a4f
Ömer Koç <63169919+omercankoc@users.noreply.github.com>
2024-10-23 14:59:28 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Printlog",
+    name: "printlog",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
